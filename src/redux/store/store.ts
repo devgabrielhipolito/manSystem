19e896638d683bf269b55e9d10bd62bdac2c6a21
gabrielhipolitoo <devgabrielhipolito>
2024-09-08 12:26:50 -0300

    feat: redux-saga

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,8 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "../reducers";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "../sagas/rootSaga";
+const sagaMiddleware = createSagaMiddleware();
+const middleware = [sagaMiddleware];
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(middleware),
 });
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
